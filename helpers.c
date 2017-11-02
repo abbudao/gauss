42198a4206d412d205bf02ec686f1bb7470e8633
Abbud-Desktop <abbudao@gmail.com>
2017-11-02 13:06:32 -0200

    Pretty priting on printMatrix

--- helpers.c
+++ helpers.c
@@ -2,10 +2,18 @@ void printMatrix(double *A, int order){
   int i, j;
   for(i=0;i<order;i++){
     for(j=0;j<order;j++){
-      printf("%lf ", A[j*order+i]);
+      if(j==0){
+        printf("|");
+      }
+      printf(" %-6.1f ", A[j*order+i]);
+      if(j==order-1){
+        printf("|");
+      }
     }
-    printf("\n" );
+      printf("\n");
   }
-  printf("-------------------\n");
+  for(i=0;i<order;i++){
+    printf("----");
+  }
+      printf("\n");
 }
-
